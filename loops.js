ff615c098cdc622966105df155912f188b0335c1
Kate Rentschler <nkpe@outlook.com>
2018-10-05 17:27:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -31,5 +31,7 @@ function doWhileLoop(array){
   do{
     array.pop();
     //incrementVariable();
-  }while(array.length > 0 || incrementVariable())
+  }while(array.length > 0 || incrementVariable());
+  
+  return array;
 }
