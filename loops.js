f212fc9ff68ac2e8f8c9dc50b751199eca9b9013
Kate Rentschler <nkpe@outlook.com>
2018-10-05 16:54:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,13 @@
-//function forLoop
+function forLoop(array){
+
+  for(var i = 0; i<25; i++ ){
+    if (i==1){
+    array.push(`I am ${i} strange loop`);
+    }
+    
+    else{
+    array.push(`I am ${i} strange loops`);
+  }
+  }
+  return array;
+}
